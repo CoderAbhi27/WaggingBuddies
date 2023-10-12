b7141f7ae6d3464ea094e280ec662cb08c26e6bf
aryan <aryanrdat@gmail.com>
2023-10-12 21:21:16 +0530

    view model shelter push

---
+++ app/src/main/java/com/example/waggingbuddies/ViewModels/RetrofitShelter/ShelterViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.waggingbuddies.ViewModels.RetrofitShelter
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.waggingbuddies.DataClass.PetsDataClass
+import retrofit2.Response
+
+class ShelterViewModel(application: Application): AndroidViewModel(application) {
+    var ShelterList = MutableLiveData<List<PetsDataClass>?>()
+    private val retrofit: RetrofitInstanceShelter = RetrofitInstanceShelter() // Initialize Retrofit here
+    private val context = getApplication<Application>().applicationContext
+
+    suspend fun getShelter()
+    {
+        Log.i("Data", "Function Called")
+        try {
+            val response: Response<List<PetsDataClass>> = retrofit.shelterApiService.getShelter()
+            if (response.isSuccessful) {
+                val data = response.body()
+                if (data != null) {
+                    // Data fetched successfully
+                  Log.d("Data", data.toString())
+                    ShelterList.postValue(data)
+
+
+                }
+            } else {
+                // Handle API error here
+                // For example, you can throw a custom exception or return an empty list
+                Log.e("API Error", "Response code: ${response.code()}")
+            }
+        }
+        catch (e:Exception)
+        {
+            Log.e("Error", e.toString())
+        }
+    }
+}
