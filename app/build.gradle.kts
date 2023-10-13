2110db5876cefdbd5cc4af2bd952cc97f82d2697
Svapak <anjalikumarimalakar22@gmail.com>
2023-10-13 09:36:10 +0530

    www

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.7.2")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
